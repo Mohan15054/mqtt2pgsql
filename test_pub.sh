8e4c8e69741f1d6536da9c3bd53d0f08f5f59f4d
Arvindh <arvindh91@gmail.com>
2024-03-15 16:53:37 +0530

    update test scripts
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- test_pub.sh
+++ test_pub.sh
@@ -8,11 +8,11 @@ mosquitto_pub -m "{'time','test_time', 'metric' : 'test_metric', 'value' : 'Inva
 echo "Publishing invalid topic length ,topic without schema and table...."
 mosquitto_pub -m "{\"time\":\"test_time\", \"metric\" : \"test_metric\", \"value\" : \"Invalid topic length ,topic without schema and table\"}"  -t "iiot"
 
-# Invalid data
-echo "Publishing invalid data 1...."
-mosquitto_pub -m "{\"time\":\"$(date '+%s')\",\"iid\":\"test_id\", \"metric\" : \"test_metric\", \"value\" : \"invalid_data_1\"}"  -t "iiot/company/public/test/metrics"
-echo "Publishing invalid data 2...."
-mosquitto_pub -m "{\"time\":\"$(date '+%s')$(date '+%N')\",\"iid\":\"test_id\", \"metric\" : \"test_metric\", \"value\" : \"invalid_data_2\"}"  -t "iiot/company/public/test/metrics"
+# Invalid time
+echo "Publishing invalid time 1...."
+mosquitto_pub -m "{\"time\":\"$(date '+%s')\",\"iid\":\"test_id\", \"metric\" : \"test_metric\", \"value\" : \"invalid time 1\"}"  -t "iiot/company/public/test/metrics"
+echo "Publishing invalid time 2...."
+mosquitto_pub -m "{\"time\":\"$(date '+%s')$(date '+%N')\",\"iid\":\"test_id\", \"metric\" : \"test_metric\", \"value\" : \"invalid time 2\"}"  -t "iiot/company/public/test/metrics"
 
 # Correct message
 echo "Publishing correct message...."
